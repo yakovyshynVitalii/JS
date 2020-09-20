d9eb0d92a663094f9add3d8d514e2867439f872c
yakovyshynVitalii <vakovisin94@gmail.com>
2020-09-20 19:37:08 +0300

    finished practic_1

---
+++ Практика_1/main.js
@@ -0,0 +1,79 @@
+// 1.Write a JavaScript program which compute, the average marks of the following students Then, 
+// this average is used to determine the corresponding grade.
+
+// let students = [
+//     student_1 = {
+//         name : "John",
+//         points : 80
+//     },
+//     student_2 = {
+//         name : "Bill",
+//         points : 77
+//     },
+//     student_3 = {
+//         name : "David",
+//         points : 68
+//     },
+//     student_4 = {
+//         name : "Ryan",
+//         points : 88
+//     },
+//     student_5 = {
+//         name : "Nick",
+//         points : 95
+//     }
+// ];
+// let rating , sum = 0, GPA;
+ 
+// console.log(students);
+
+// for (let i = 0; i < students.length; i++){
+
+//     if (students[i].points < 60){
+//         rating = "F";
+//         console.log(`${students[i].name} отримав балів ${students[i].points} тому оцінка ${rating}`);
+//     } else if (students[i].points < 70) {
+//         rating = "D";
+//         console.log(`${students[i].name} отримав балів ${students[i].points} тому оцінка ${rating}`);
+//     }else if( students[i].points < 80){
+//         rating = "C";
+//         console.log(`${students[i].name} отримав балів ${students[i].points} тому оцінка ${rating}`);
+//     }else if( students[i].points < 90){
+//         rating = "B";
+//         console.log(`${students[i].name} отримав балів ${students[i].points} тому оцінка ${rating}`);
+//     }else if( students[i].points < 100){
+//         rating = "A";
+//         console.log(`${students[i].name} отримав балів ${students[i].points} тому оцінка ${rating}`);
+//     }
+// }
+// for (let j = 0 ; j < students.length; j++ ){
+//     sum  += students[j].points;
+// }
+// GPA = sum / students.length;
+// console. log (`Середній бал групи ${GPA}`);
+
+
+
+// 2.Write a JavaScript program to sum the multiples of 3 or 5 under 1000.
+
+// let number = 0;
+
+// for (i = 0; i <= 1000; i++){
+//     console.log (i);
+//     if ( i % 3 == 0 || i % 5 ==0){
+//         number += i
+//     }
+// }
+// console.log (number);
+
+
+// 3. Write a JavaScript program to construct the following pattern, using a nested for loop.
+
+// let element ='';
+
+// for ( i= 0 ; i < 5 ; i++){
+//     element += '*';
+//     console.log (element);
+// }
+
+
